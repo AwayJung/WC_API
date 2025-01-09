404fa1c058b2b831aab551ba4c6c5ff322fce121
wonjung <wonjung67@gmail.com>
2025-01-09 16:57:01 +0900

    git init

---
+++ src/test/java/org/zerock/wc_api/WcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.wc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
